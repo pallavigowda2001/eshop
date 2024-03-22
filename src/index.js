218f250644f8a4c567cd07448bfa2df60a4bfed7
pallavigowda2001 <pallavigowda78197@gmail.com>
2024-03-22 14:00:22 +0530

    eshop

--- src/index.js
+++ src/index.js
@@ -9,11 +9,14 @@ import "./media.css"
 
 import "bootstrap-icons/font/bootstrap-icons.css"
 import "react-toastify/dist/ReactToastify.css"
+import AuthProvider from './Context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <AuthProvider>
+          <App />
+      </AuthProvider>
   </React.StrictMode>
 );
 
